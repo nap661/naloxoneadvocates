bdee1fc81198b14ca3fddc0861a8dd5ddef07e07
nap661 <kelly.anne1@nhs.net>
2026-02-16 07:15:04 +0000

    Delete templates-about.php

--- templates-about.php
+++
@@ -1,7 +0,0 @@
-<?php /* Template Name: About (Prebuilt) */ if (!defined('ABSPATH')) exit; get_header(); ?>
-<section class="section"><div class="container">
-  <h1>About Us</h1>
-  <p>We are a grassroots, peer‑led harm reduction organisation dedicated to reducing drug‑related deaths.</p>
-  <p>We deliver naloxone training, community outreach and P2P education across Plymouth and Devon.</p>
-</div></section>
-<?php get_footer(); ?>
