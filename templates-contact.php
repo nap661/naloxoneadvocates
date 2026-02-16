f3d2112abc5628358666ae8bea40f984e4962d96
nap661 <kelly.anne1@nhs.net>
2026-02-16 07:19:20 +0000

    Delete templates-contact.php

--- templates-contact.php
+++
@@ -1,7 +0,0 @@
-<?php /* Template Name: Contact (Prebuilt) */ if (!defined('ABSPATH')) exit; get_header(); ?>
-<section class="section"><div class="container">
-  <h1>Contact Us</h1>
-  <p>Emergency: <strong>999</strong><br>Phone: 01752 000 000<br>Email: <a href="mailto:info@naloxoneadvocates.org">info@naloxoneadvocates.org</a></p>
-  <div class="note">Add a form here with WPForms / Forminator once installed.</div>
-</div></section>
-<?php get_footer(); ?>
